d4ce7dd956bc07a09a6911eed02d6d31d80697ee
Mateo <teo9515@gmail.com>
2025-04-24 17:39:11 -0500

    panel-admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import CardAdmin from "../components/CardAdmin";
+
 export default function Home() {
   return (
-    <div className="grid text-white bg-red-900 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main>
-        <h1>Hola</h1>
-      </main>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-white  w-full">
+      <div className="w-5/6 space-y-10 lg:w-4/6">
+        <h1 className="text-5xl font-bold text-[#163020] text-center">
+          Panel Admin
+        </h1>
+        <p className="text-[#163020] text-center text-xl lg:text-2xl ">
+          Accede a la administración de clientes y guarderías. Gestiona toda la
+          información de forma sencilla y rápida.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6  w-full">
+          <CardAdmin
+            title="Clientes"
+            description="Administra la información de tus clientes, visualiza sus datos y gestiona sus registros fácilmente."
+            href="/clients"
+            icon="👤"
+          />
+          <CardAdmin
+            title="Guarderías"
+            description="Consulta y administra las guarderías disponibles, horarios, capacidad y más."
+            href="/guarderias"
+            icon="🏠"
+          />
+        </div>
+      </div>
+    </main>
   );
 }
