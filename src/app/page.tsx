db0f0f099004fb53100af6e18cf40bf892178f11
Mateo <teo9515@gmail.com>
2025-04-29 17:57:23 -0500

    fix responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,30 @@ import CardAdmin from "../components/CardAdmin";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-white  w-full">
-      <div className="w-5/6 space-y-10 lg:w-4/6">
+    <main className="flex flex-col  min-h-screen py-10 w-full lg:w-5/6 justify-center items-center gap-5">
+      <div className=" w-5/6 space-y-5 ">
         <h1 className="text-5xl font-bold text-[#163020] text-center">
           Glamto
         </h1>
-        <h3 className="text-4xl font-bold text-[#163020] text-center">
+        <h3 className="text-3xl font-bold text-[#163020] text-center">
           Panel Admin
         </h3>
         <p className="text-[#163020] text-center text-xl lg:text-2xl ">
           Accede a la administración de clientes y guarderías. Gestiona toda la
           información de forma sencilla y rápida.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6  w-full">
-          <CardAdmin
-            title="Clientes"
-            description="Administra la información de tus clientes, visualiza sus datos y gestiona sus registros fácilmente."
-            href="/clients"
-            icon="👤"
-          />
-          <CardAdmin
-            title="Guarderías"
-            description="Consulta y administra las guarderías disponibles, horarios, capacidad y más."
-            href="/guarderias"
-            icon="🏠"
-          />
-        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-5/6">
+        <CardAdmin
+          title="Clientes"
+          description="Administra la información de tus clientes, visualiza sus datos y gestiona sus registros fácilmente."
+          href="/clients"
+        />
+        <CardAdmin
+          title="Guarderías"
+          description="Consulta y administra las guarderías disponibles, horarios, capacidad y más."
+          href="/guarderias"
+        />
       </div>
     </main>
   );
--- src/components/CardAdmin.tsx
+++ src/components/CardAdmin.tsx
@@ -4,21 +4,22 @@ interface CardAdminProps {
   title: string;
   description: string;
   href: string;
-  icon?: string; // opcional si quieres personalizar el icono
 }
 
 export default function CardAdmin({
   title,
   description,
   href,
-  icon = "👤",
 }: CardAdminProps) {
   return (
     <Link href={href}>
-      <div className="bg-[#AEBD77] rounded-2xl shadow-md shadow-black w-full max-w-2xl h-60 p-6 text-center hover:shadow-lg hover:scale-101 transition-shadow cursor-pointer">
-        <div className="text-4xl mb-4">{icon}</div>
-        <h2 className="text-3xl text-[#163020] font-semibold mb-2">{title}</h2>
-        <p className="text-[#163020] text-xl">{description}</p>
+      <div className="bg-[#AEBD77] flex flex-col justify-center items-center rounded-2xl shadow-md shadow-black w-full max-w-2xl h-48 lg:w-10/12 text-center hover:shadow-lg hover:scale-101 transition-shadow cursor-pointer">
+        <div className="w-10/12">
+          <h2 className="text-3xl text-[#163020] font-semibold mb-2">
+            {title}
+          </h2>
+          <p className="text-[#163020] text-xl text-justify">{description}</p>
+        </div>
       </div>
     </Link>
   );
